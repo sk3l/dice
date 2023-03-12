903471a0d3790bf8e95c08c273b80224315a301c
sk3l <admin@skelton.onl>
2023-03-12 14:26:14 -0400

    Update entry-point.sh
    Change start directory to /home/mskelton for tmux entry point script

--- install/entry-point.sh
+++ install/entry-point.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-tmux -2 new-session -s DICE -d  -n "local" -c ~/code/
-tmux -2 split-window  -v -c ~/code/ -t 1
-tmux -2 split-window  -v -c ~/code/ -t 2 vifm
-#tmux -2 kill-pane -t 1
+tmux -2 new-session -s DICE -d  -n "local" -c ~/
+tmux -2 split-window  -v -c ~/ -t 1
+tmux -2 split-window  -v -c ~/ -t 2 vifm
+tmux -2 kill-pane -t 1
 
-tmux -2 new-window -n "code"    -c ~/code/
-tmux -2 new-window -n "build"   -c ~/code/
-tmux -2 new-window -n "test"    -c ~/code/
-tmux -2 new-window -n "run"     -c ~/code/
+tmux -2 new-window -n "code"    -c ~/
+tmux -2 new-window -n "build"   -c ~/
+tmux -2 new-window -n "test"    -c ~/
+tmux -2 new-window -n "run"     -c ~/
 
 tmux -2 select-window -t 1
 
